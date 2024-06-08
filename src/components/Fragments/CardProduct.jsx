42e2e42a7a907dfd4e707af699f1f3868bb26487
Dian-indah <dianzurindah.aks17@gmail.com>
2024-06-08 12:29:41 +0700

    8.Nested Component

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -1,37 +1,50 @@
-const CardProduct = () => {
+import { children } from "react";
+import Button from "../Elements/Button";
+
+const CardProduct = (props) => {
+  const { children } = props;
   return (
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">
-        <a href="#">
-          <img
-            src="/images/shoes-1.jpg"
-            alt="product"
-            className="p-8 rounded-t-lg"
-          />
-        </a>
-        <div className="px-5 pb-5">
-          <a href="#">
-            <h5 className="text-xl font-semibold tracking-tight text-white">
-              Sepatu Baru
-            </h5>
-            <p className="text-s text-white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
-              magni deleniti. Maxime, libero vitae. Officia aut consectetur,
-              nulla, error voluptatum inventore, iure vel minus nisi ullam
-              cumque. Aliquam, quam esse.
-            </p>
-          </a>
-        </div>
-        <div className="flex items-center justify-between px-5 pb-5">
-            <span className="text-xl font-bold text-white">Rp.1.000.000</span>
-            <Button classname="bg-blue-600">Add To Cart</Button>
-        </div>
-      </div>
+    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2">
+      {children}
+    </div>
   );
 };
 
-const Body = () => {
-    return (
-        
-    )
-}
+const Body = (props) => {
+  const { title, children } = props;
+  return (
+    <div className="px-5 pb-5">
+      <a href="#">
+        <h5 className="text-xl font-semibold tracking-tight text-white">
+          {title}
+        </h5>
+        <p className="text-s text-white">{children}</p>
+      </a>
+    </div>
+  );
+};
+
+const Header = (props) => {
+  const { image } = props;
+  return (
+    <a href="#">
+      <img src={image} alt="product" className="p-8 rounded-t-lg" />
+    </a>
+  );
+};
+
+const Footer = (props) => {
+  const {price} = props
+  return (
+    <div className="flex items-center justify-between px-5 pb-5">
+      <span className="text-xl font-bold text-white">{price}</span>
+      <Button classname="bg-blue-600">Add To Cart</Button>
+    </div>
+  );
+};
+
+CardProduct.Header = Header;
+CardProduct.Body = Body;
+CardProduct.Footer = Footer;
+
 export default CardProduct;
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,9 +1,30 @@
 import Button from "../components/Elements/Button";
+import CardProduct from "../components/Fragments/CardProduct";
 
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      
+      {/* <h1>test</h1> */}
+      <CardProduct>
+        <CardProduct.Header image="/images/shoes-1.jpg"/>
+        <CardProduct.Body title="Sepatu Baru">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, magni
+          deleniti. Maxime, libero vitae. Officia aut consectetur, nulla, error
+          voluptatum inventore, iure vel minus nisi ullam cumque. Aliquam, quam
+          esse.
+        </CardProduct.Body>
+        <CardProduct.Footer price="Rp.1.500.000"/>
+      </CardProduct>
+      <CardProduct>
+        <CardProduct.Header image="/images/shoes-1.jpg"/>
+        <CardProduct.Body title="Sepatu Baru">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, magni
+          deleniti. Maxime, libero vitae. Officia aut consectetur, nulla, error
+          voluptatum inventore, iure vel minus nisi ullam cumque. Aliquam, quam
+          esse.
+        </CardProduct.Body>
+        <CardProduct.Footer price="Rp.1.500.000"/>
+      </CardProduct>
     </div>
   );
 };
