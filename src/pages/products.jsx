f6113c123de85fc75f40abaca194cefbbe986985
Dian-indah <dianzurindah.aks17@gmail.com>
2024-09-10 10:11:04 +0700

    16. Get Data API

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -2,39 +2,41 @@ import { Fragment, useEffect, useRef, useState } from "react";
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
 import Counter from "../components/Fragments/Counter";
+import { getProducts } from "../services/product.service";
 
-const products = [
-  {
-    id: 1,
-    name: "Sepatu Baru",
-    price: 1500000,
-    image: "/images/shoes-1.jpg",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, magni
-    deleniti. Maxime, libero vitae. Officia aut consectetur, nulla, error
-    voluptatum inventore, iure vel minus nisi ullam cumque. Aliquam, quam
-    esse.`,
-  },
-  {
-    id: 2,
-    name: "Sepatu Lama",
-    price: 1000000,
-    image: "/images/shoes-1.jpg",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, magni
-    deleniti. `,
-  },
-  {
-    id: 3,
-    name: "Sepatu Lalala",
-    price: 2000000,
-    image: "/images/shoes-1.jpg",
-    description: `ini merupakan sepatu lalala`,
-  },
-];
+// const products = [
+//   {
+//     id: 1,
+//     name: "Sepatu Baru",
+//     price: 1500000,
+//     image: "/images/shoes-1.jpg",
+//     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, magni
+//     deleniti. Maxime, libero vitae. Officia aut consectetur, nulla, error
+//     voluptatum inventore, iure vel minus nisi ullam cumque. Aliquam, quam
+//     esse.`,
+//   },
+//   {
+//     id: 2,
+//     name: "Sepatu Lama",
+//     price: 1000000,
+//     image: "/images/shoes-1.jpg",
+//     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, magni
+//     deleniti. `,
+//   },
+//   {
+//     id: 3,
+//     name: "Sepatu Lalala",
+//     price: 2000000,
+//     image: "/images/shoes-1.jpg",
+//     description: `ini merupakan sepatu lalala`,
+//   },
+// ];
 
 const email = localStorage.getItem("email");
 const ProductsPage = () => {
   const [cart, setCart] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [products, setProducts] = useState([]);
 
   //ambil data dari local storage -- menggunakan didmount --
   useEffect(() => {
@@ -43,9 +45,15 @@ const ProductsPage = () => {
     );
   }, []);
 
+  useEffect(() => {
+    getProducts((data) => {
+      setProducts(data);
+    });
+  },[]);
+
   //menghitung total price -- menggunakan didupdate --
 useEffect(() => {
-  if(cart.length > 0) {
+  if(products.length > 0 && cart.length > 0) {
     const sum = cart.reduce((acc,item) => {
       const product = products.find((product) => product.id === item.id);
       return acc + product.price * item.qty;
@@ -53,7 +61,7 @@ useEffect(() => {
     setTotalPrice(sum);
     localStorage.setItem("cart",JSON.stringify(cart)); 
   }
-}, [cart]);
+}, [cart, products]);
 
   const handleLogout = () => {
     localStorage.removeItem("email");
@@ -71,13 +79,6 @@ useEffect(() => {
     } else {
       setCart([...cart, { id, qty: 1 }]);
     }
-    // setCart([
-    //   ...cart,
-    //   {
-    //     id,
-    //     qty: 1,
-    //   },
-    // ]);
   };
 
   //useRef
@@ -108,10 +109,10 @@ useEffect(() => {
       </div>
       <div className="flex justify-center py-5">
         <div className="w-4/6 flex flex-wrap">
-          {products.map((product) => (
+          {products.length > 0 && products.map((product) => (
             <CardProduct key={product.id}>
               <CardProduct.Header image={product.image} className="h-64" />
-              <CardProduct.Body name={product.name}>
+              <CardProduct.Body name={product.title}>
                 {product.description}
               </CardProduct.Body>
               <CardProduct.Footer
@@ -134,7 +135,7 @@ useEffect(() => {
               </tr>
             </thead>
             <tbody>
-              {cart.map((item) => {
+              {products.length > 0 && cart.map((item) => {
                 const product = products.find(
                   (product) => product.id === item.id
                 );
@@ -143,11 +144,11 @@ useEffect(() => {
                 }
                 return (
                   <tr key={item.id}>
-                    <td>{product.name}</td>
+                    <td>{product.title.substring(0, 10)}...</td>
                     <td>
                       {product.price.toLocaleString("id-ID", {
                         style: "currency",
-                        currency: "IDR",
+                        currency: "USD",
                       })}
                       {/* {product.price.toLocaleString('id-ID', {style:'currency', currency: 'IDR'})} */}
                     </td>
@@ -156,7 +157,7 @@ useEffect(() => {
                       {/* {product.price * item.qty} */}
                       {(item.qty * product.price).toLocaleString("id-ID", {
                         style: "currency",
-                        currency: "IDR",
+                        currency: "USD",
                       })}
                     </td>
                   </tr>
@@ -170,7 +171,7 @@ useEffect(() => {
                   <b>
                     {totalPrice.toLocaleString("id-ID", {
                       style: "currency",
-                      currency: "IDR",
+                      currency: "USD",
                     })}
                   </b>
                 </td>
